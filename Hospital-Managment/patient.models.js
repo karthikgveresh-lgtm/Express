5155b895ca34716ee210923739c7b863ba984cd8
karthikgveresh-Igtm <karthikgveresh@gmail.com>
2026-08-14 13:14:46 +0530

    feat: initialize Doctor and Patient mongoose models

--- Hospital-Managment/patient.models.js
+++ Hospital-Managment/patient.models.js
@@ -1,5 +1,42 @@
 import mongoose from 'mongoose'
 
-const patientSchema = new mongoose.Schema({}, { timestaps: true })
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    diagonsedWith: {
+        type: String,
+        required: true
+    },
+
+    address: {
+        type: String,
+        required: true
+    },
+
+    age: {
+        type: Number,
+        required: true
+    },
+
+    gender: {
+        type: String,
+        enum: ["MALE", "FEMALE", "OTHERS"],
+        required: true
+    },
+
+    bloodGroup: {
+        type: String,
+        required: true
+    },
+
+    admittedIn: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital"
+    }
+
+}, { timestaps: true })
 
 export const Patient = mongoose.model("Patient", patientSchema)
